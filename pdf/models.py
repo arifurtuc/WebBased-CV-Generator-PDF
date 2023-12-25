d53088fda23ed93a8a093d374743db43e59936f6
arif <arifur.tuc@gmail.com>
2023-12-25 19:56:19 +0100

    Add comments

--- pdf/models.py
+++ pdf/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 
+# Defining the Profile model for storing CV-related information
 class Profile(models.Model):
+    # Fields to store personal information and CV details
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
@@ -11,6 +13,8 @@ class Profile(models.Model):
     university = models.CharField(max_length=200)
     work = models.TextField(max_length=2000)
     skills = models.TextField(max_length=20000)
+
+    # Fields for tracking creation and update timestamps
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
--- pdf/views.py
+++ pdf/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from .models import Profile
 
 
+# View function for form submission
 def accept(request):
     if request.method == 'POST':
+        # Get data from the POST request
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
         phone = request.POST.get('phone', '')
@@ -14,11 +16,14 @@ def accept(request):
         work = request.POST.get('work', '')
         skills = request.POST.get('skills', '')
 
+        # Create a Profile object with the obtained data
         profile = Profile(
             name=name, email=email, phone=phone, summary=summary,
             degree=degree, school=school, university=university, work=work,
             skills=skills
         )
+
+        # Save the Profile object to the database
         profile.save()
 
     return render(request, 'pdf/accept.html')
