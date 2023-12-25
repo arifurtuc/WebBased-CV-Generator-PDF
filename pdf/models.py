2f8c1a40988885a0a30b035c690c8a218cddc0b3
arif <arifur.tuc@gmail.com>
2023-12-25 15:07:59 +0100

    Add profile model

--- pdf/models.py
+++ pdf/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    summary = models.TextField(max_length=2000)
+    degree = models.CharField(max_length=200)
+    school = models.CharField(max_length=200)
+    university = models.CharField(max_length=200)
+    work = models.TextField(max_length=2000)
+    skills = models.TextField(max_length=20000)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
